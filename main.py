14b380cea44b22a145b6b8e0c227a07256e4279c
Dean Logan <deanlogan42@gmail.com>
2024-06-03 15:51:59 +0100

    Get dates for new movies

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
 import os
+import time
 import smtplib
+from datetime import datetime
 from selenium import webdriver
 from dotenv import load_dotenv
 from email.mime.text import MIMEText
@@ -8,8 +9,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 
-def search_cinema(location):
-    driver = webdriver.Firefox()
+def search_cinema(driver, location):
     driver.get('https://www.omniplex.ie/whatson')
     # click cookie agreement
     print("clicking cookie agreement")
@@ -25,8 +25,8 @@ def search_cinema(location):
     return movies_on_website
 
 
-def get_diff_movies(location):
-    movies_on_website = search_cinema(location)
+def get_diff_movies(driver, location):
+    movies_on_website = search_cinema(driver, location)
     movies_on_file = read_file_to_arr("movie_list_"+location+".txt")
     return [movie for movie in movies_on_website if movie not in movies_on_file]
 
@@ -67,12 +67,36 @@ def send_email(recipient, body):
     smtp.sendmail(sender, [recipient], message.as_string())
     smtp.quit()
 
+def format_movie_title_to_link(movie_title):
+    movie_title = movie_title.replace(" ", "-").lower()
+    movie_title = movie_title.replace(":", "")
+    movie_title = movie_title.replace(")", "-")
+    movie_title = movie_title.replace("(", "")
+    return movie_title
+
+def get_movie_dates(driver, location, movie_title):
+    movie_title_link = format_movie_title_to_link(movie_title)
+    driver.get('https://www.omniplex.ie/whatson/movie/showtimes/'+movie_title_link)
+    select_dropdown_option(driver, 'homeSelectCinema', location)
+    print("clicking date selection")
+    wait_and_click(driver, By.XPATH, '/html/body/div[9]/div[1]/div/div/div/div/div/a[8]/span')
+    print("clicked date selection")
+    dates = driver.find_elements(by=By.CSS_SELECTOR, value=".picker__day.picker__day--infocus:not([aria-disabled='true'])")
+    available_dates = []
+    for date in dates:
+        dateObj = datetime.fromtimestamp(int(date.get_attribute('data-pick')) / 1000)
+        available_dates.append(dateObj.strftime('%d %b %Y'))
+    print(available_dates)
+
 def main():
+    driver = webdriver.Firefox()
     location = "carrickfergus"
-    diff_movies = get_diff_movies(location)
+    diff_movies = get_diff_movies(driver, location)
     if diff_movies:
-        write_arr_to_file(diff_movies, "movie_list_"+location+".txt")
-        send_email(os.environ.get('RECIPIENT'), "New movies available: " + ', '.join(diff_movies))
+        get_movie_dates(driver, location, diff_movies[0])
+        # write_arr_to_file(diff_movies, "movie_list_"+location+".txt")
+        # send_email(os.environ.get('RECIPIENT'), "New movies available: " + ', '.join(diff_movies))
+    driver.close()
 
 if __name__ == '__main__':
     main()
