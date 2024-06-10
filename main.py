0f752f3c37f5ec1d1e02a2e0a5825b650e0fb104
Dean Logan <deanlogan42@gmail.com>
2024-06-10 13:19:40 +0100

    Creating code to run on raspberry pi to host it locally instead of the cloud

--- main.py
+++
@@ -1,8 +0,0 @@
-def lambda_handler(event, context):
-    return {
-        'statusCode': 200,
-        'body': 'Success, this ran'
-    }
-
-if __name__ == '__main__':
-    lambda_handler(None, None)
